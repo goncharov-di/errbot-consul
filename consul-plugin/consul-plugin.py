ff75ae9ef87c307001afe2b92f03e5d47a06fd99
Dmitry Goncharov <Dmitry.Goncharov@returnonintelligence.com>
2017-12-28 17:08:21 +0300

    Remove "versions" from path

--- consul-plugin/consul-plugin.py
+++ consul-plugin/consul-plugin.py
@@ -9,7 +9,7 @@ class ConsulPlugin(BotPlugin):
     def consul_get_value(self, msg, branch=None, key=None):
         """Get value for particular branch and key from Consul."""
         c = consul.Consul(host=CONSUL_HOST)
-        key = 'versions/'+branch+'/'+key
+        key = branch + '/' + key
         index, data = c.kv.get(key)
         v = data['Value']
         yield ('{}:{}'.format(key, v.decode("utf-8")))
@@ -20,7 +20,7 @@ class ConsulPlugin(BotPlugin):
     def consul_copy_branch(self, msg, source=None, destination=None):
         """Copy one Consul branch to another."""
         c = consul.Consul(host=CONSUL_HOST)
-        key = 'versions/' + source + '/'
+        key = source + '/'
         index, data = c.kv.get(key, recurse=True)
         for d in data:
             key = d['Key']
@@ -33,7 +33,7 @@ class ConsulPlugin(BotPlugin):
     def consul_list_branch(self, msg, branch=None):
         """List values for particular branch in Consul."""
         c = consul.Consul(host=CONSUL_HOST)
-        key = 'versions/' + branch + '/'
+        key = branch + '/'
         index, data = c.kv.get(key, recurse=True)
         for a in data:
             yield ('{}:{}'.format(a['Key'], a['Value'].decode("utf-8")))
@@ -45,5 +45,5 @@ class ConsulPlugin(BotPlugin):
     def consul_update_value(self, msg, branch=None, key=None, value=None):
         """Update value of particular key in Consul"""
         c = consul.Consul(host=CONSUL_HOST)
-        key = 'versions/' + branch + '/' + key
+        key = branch + '/' + key
         c.kv.put(key, value)
